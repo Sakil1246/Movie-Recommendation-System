c7e9475a8465457772be4e1cd6a70cd51deb6b38
Sakil1246 <sakilahmed345677@gmail.com>
2025-04-02 19:19:28 +0530

    update constant

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,12 +1,10 @@
 
-import dotenv from 'dotenv'
-dotenv.config()
 export const bgUrl="https://assets.nflxext.com/ffe/siteui/vlv3/42a0bce6-fc59-4c1c-b335-7196a59ae9ab/web_tall_panel/IN-en-20250303-TRIFECTA-perspective_8d2f60cf-007f-4f25-99b0-7c0b77f38bc1_large.jpg"
 
 export const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: process.env.TOKEN,
+      Authorization: import.meta.env.TOKEN,
     }
   };
